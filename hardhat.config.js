574b65adadb0322c6d44d33dccd36a78d47160fd
Maxim Chirosca <maximc.plusplus@gmail.com>
2021-01-29 13:19:54 -0500

    hardhat env setup, install oz contracts package

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,39 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+require("dotenv").config();
 require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-web3");
 
 module.exports = {
-  solidity: "0.7.0",
+  networks: {
+    hardhat: {
+      // Uncomment these lines to use mainnet fork
+      // forking: {
+      //   url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      // blockNumber: 11589707,
+      // },
+    },
+    live: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [process.env.MAINNET_PRIVKEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
+    // url: "https://api-rinkeby.etherscan.io/",
+  },
+  solidity: {
+    version: "0.7.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  mocha: {
+    timeout: 240000,
+  },
 };
